b4b36efad61060a7d8e92ba5afcd21cb1ed82910
HyeongJu Yoo <zerofunc@naver.com>
2019-01-26 23:30:38 +0900

    Create GenericFunctional

---
+++ Java/EffectiveJava3/Chapter5/GenericFunctional.java
@@ -0,0 +1,39 @@
+package effectivejava.chapter5.item30;
+
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class GenericFunctional {
+    private <T> void merge(T t1, T t2, BiFunction<T, T, Boolean> biFunction){
+        if(biFunction.apply(t1, t2)) {
+            doSomething();
+        } else {
+            nothing();
+        }
+    }
+
+    private void nothing() {
+        System.out.println("Nothing");
+    }
+
+    private void doSomething() {
+        System.out.println("Do Something");
+    }
+
+
+    private void integersMerge() {
+        BiFunction<Integer, Integer, Boolean> integerIntegerObjectBiFunction = (Integer a, Integer b) -> {
+            return a > b;
+        };
+
+        merge(2, 5, integerIntegerObjectBiFunction);
+    }
+
+    private void booleanMerge() {
+        BiFunction<Boolean, Boolean, Boolean> booleanBiFunction = (Boolean a, Boolean b) -> {
+            return a.equals(b);
+        };
+
+        merge(true, true, booleanBiFunction);
+    }
+}
